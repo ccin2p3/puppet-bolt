1eae07fe60aa5d2340d2b9a121cb74abea7e16b8
Remi Ferrand <remi.ferrand@cc.in2p3.fr>
2017-10-12 10:43:38 -0700

    Based on Josh Cooper suggestion, simplify the patch for GSS-API
    * Specifying auth_methods is not really required as long as you specify
      PreferredAuthentications in your ssh_config file

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,8 @@ class bolt(
   String $package_provider = 'puppet_gem',
   String $hack_script_path = '/var/tmp/hack_bolt_script.sh',
   Array[String] $bolt_ssh_auth_methods = ['gssapi-with-mic', 'publickey', 'password', 'keyboard-interactive'],
-  Boolean $hack_bolt_to_allow_ssh_gssapi = false
+  Boolean $hack_bolt_to_allow_ssh_gssapi = false,
+  Boolean $explicitly_specify_bolt_ssh_auth_methods = false
 )
 {
   package { $package_name:
@@ -27,7 +28,7 @@ class bolt(
       content => template("${module_name}/bolt_support_gssapi_hack.erb")
     }
 
-    -> exec { 'hack bolt to add ssh gss-api support':
+    ~> exec { 'hack bolt to add ssh gss-api support':
       command     => $hack_script_path,
       refreshonly => true,
       subscribe   => Package[$package_name]
